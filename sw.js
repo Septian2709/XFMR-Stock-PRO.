f48be69c5a9a45d9f028f649a8440a0668464f07
Septian2709 <septianz.asmaulhaq@gmail.com>
2026-07-03 13:53:04 +0700

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'stock-manager-v1';
+const urlsToCache = [
+    '/',
+    'index.html',
+    'css/style.css',
+    'js/app.js',
+    'js/spreadsheet.js',
+    'manifest.json',
+    'images/icon-192x192.png',
+    'images/icon-512x512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Strategy - Cache First
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then(response => {
+                        // Check if valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    });
+            })
+    );
+});
